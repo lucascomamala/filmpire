d57bec5ef70938e181df730f154bc36d17ed0005
Lucas <lucascomamala@gmail.com>
2023-08-26 01:47:12 -0600

    @mui/styles install

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import { CssBaseline } from "@mui/material"
 import { Route, Routes } from "react-router-dom"
 
+import useStyles from "./styles"
+
 import { Actors, Movies, MovieInformation, Profile } from "./pages"
 import { Navbar } from "./components"
 
 
 const App = () => {
+  const classes = useStyles()
+
   return (
-    <div id='app'>
+    <div id='app' className={classes.root}>
       <CssBaseline />
       <Navbar />
-      <main>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
         <Routes>
           <Route path="/" element={<Movies />} />
           <Route path="/actors/:id" element={<Actors />} />
