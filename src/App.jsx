387c04ecc8eee2ec39214260aff7d6f24eaece3e
Lucas <lucascomamala@gmail.com>
2023-08-26 01:40:59 -0600

    Routes and empty pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import { CssBaseline } from "@mui/material"
+import { Route, Routes } from "react-router-dom"
+
+import { Actors, Movies, MovieInformation, Profile } from "./pages"
+import { Navbar } from "./components"
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div id='app'>
+      <CssBaseline />
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Movies />} />
+          <Route path="/actors/:id" element={<Actors />} />
+          <Route path="/movies/:id" element={<MovieInformation />} />
+          <Route path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </main>
+    </div>
   )
 }
 
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Actors } from './actors/index'
+export { default as MovieInformation } from './movie/index'
+export { default as Profile } from './profile/index'
+export { default as Movies } from './movies/index'
