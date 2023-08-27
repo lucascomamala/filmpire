fa17aac66043cd4cc004d763ec68225442173d0d
Lucas <lucascomamala@gmail.com>
2023-08-27 01:10:58 -0600

    get movie query

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -32,6 +32,10 @@ export const tmdbApi = createApi({
         return `/movie/popular?page=${page}&api_key=${tmdbApiKey}`;
       },
     }),
+    // Get Movie by ID
+    getMovie: builder.query({
+      query: (id) => `/movie/${id}?append_to_response=videos,credits&api_key=${tmdbApiKey}`,
+    }),
   }),
 })
 
@@ -39,4 +43,5 @@ export const tmdbApi = createApi({
 export const {
   useGetMoviesQuery,
   useGetGenresQuery,
+  useGetMovieQuery,
 } = tmdbApi
